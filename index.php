89877ef4a89818017a3693b295ea2cfbaa0eb89a
MoztiJR10 <santiago.ariel.paz04@hotmail.com>
2021-01-07 15:41:17 -0300

    Estableci las bases

--- index.php
+++ index.php
@@ -4,10 +4,36 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="css/estilos.css">
-	<title>Document</title>
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@1,700&display=swap" rel="stylesheet">
+	<title>Cronometro</title>
 </head>
 <body>
-	<h1>Prueba y Modificacion 1 :)</h1>
+	<h1 class="coming">COMING SOON</h1>
+
+	<p class="p-will">We will be back in...</p>
+
+	<main id="main-container">
+		<div class="">
+			<h1 class="header-number__days"></h1>
+			<p></p>
+		</div>
+		<div>
+			<h1 class="header-number__hours"></h1>
+			<p></p>
+		</div>
+		<div>
+			<h1 class="header-number__minutes"></h1>
+			<p></p>
+		</div>
+		<div>
+			<h1 class="header-number__second"></h1>
+			<p></p>
+		</div>
+	</main>
+
 	<script src="js/app.js"></script>
+
 </body>
 </html>
